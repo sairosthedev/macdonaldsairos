d68afedd076771556169ca868ec78256022e3a5d
[miccx-24] <[macdonaldsairos24@gmail.com]>
2024-09-19 11:53:11 +0200

    4th commit

---
+++ src/app/api/send/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.re_6bF5KR1b_QK1RrBfRBW2dF5cJztUvfcho);
+const fromEmail = process.env.FROM_EMAIL;
+
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
